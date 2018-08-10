a430a3a925074b61b16a182b6b82fd9efd5d9296
Tomoki Sugiura @ Artificial Retina Development Office <tomoki_sugiura@nidek.co.jp>
2018-08-10 11:14:13 +0900

    mod setup.sh: add extra process to nvim

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -u
 
-for F in `ls -1` 
-do
-  if [ $F != ${0##*/} ] ; then
-    ln -s `pwd`/$F ~/.$F
-  fi
+# gen sym-links of dotfiles
+for F in $(find . -maxdepth 1 -type f); do
+  # if [ $F != ${0##*/} ] ; then
+  #   ln -s $(pwd)/$F ~/.$F
+  # fi
+  echo $F
 done
+
+# set-up config files for neovim
+ln -s $(pwd)/vim ~/.config/nvim
 
 
